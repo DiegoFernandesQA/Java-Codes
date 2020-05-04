e5e37e10bb2f770257998d62f608e1928c3f042a
Diego Fernandes <55892304+MyDevNote@users.noreply.github.com>
2020-05-04 19:06:28 -0300

    Update DoWhile.java

--- Estruturas de Repetição/DoWhile.java
+++ Estruturas de Repetição/DoWhile.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dowhile;
 
 import java.util.Scanner;
@@ -13,9 +8,7 @@ import java.util.Scanner;
  */
 public class DoWhile {
 
-    /**
-     * @param args the command line arguments
-     */
+  
     public static void main(String[] args) {
         // TODO code application logic here
         int n, cont=0, acum=0, media;
