4307e0c1eebe1c8144cdf2c49960021f6316842d
Diego Fernandes <diegofernandes62@hotmail.com>
2020-05-07 11:31:17 -0300

    Estruturas de Repetição

---
+++ Estruturas de Repetição/RepeticaoFor.java
@@ -0,0 +1,20 @@
+
+package repeticaofor;
+
+/**
+ *
+ * @author diego.fernandes
+ */
+public class RepeticaoFor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (int cc= 0; cc<=100; cc+=10) {
+            System.out.println(cc);
+        }
+    }
+    
+}
---
+++ Estruturas de Repetição/While Simples.java
@@ -0,0 +1,25 @@
+
+package mydevnote;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author diego.fernandes
+ */
+public class MyDevnote {
+
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        
+        
+        double n, acum;
+        
+    for(int cont = 1; cont<=10; ++cont){
+        System.out.printf("Digite um número ineiro: ");
+        n = ler.nextDouble();
+    }
+        System.out.println();
+    
+       }
+    
+}
